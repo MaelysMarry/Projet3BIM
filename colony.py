ec7ead8151c2cdc4a7f4c6d337ba3df96dc9e52b
Vincent Le Goff <vb.legoff@free.fr>
2020-02-19 13:16:10 +0100

    Added updatePop and run functions

--- colony.py
+++ colony.py
@@ -1,4 +1,40 @@
-import bacteria
+from bacteria import bacteria
 
 
-class colony
+class colony:
+
+    def __init__(self, nutr, Pdeath, Pdivmax, Kx, length, resist, tol):
+
+        self.nutr = nutr
+        self.Pdeath = Pdeath
+        self.Pdivmax = Pdivmax
+        self.Kx = Kx, self.length = length
+        self.resist = resist
+        self.tol = tol
+
+        self.pop = []
+
+        for i in range(length):
+
+            self.pop.append(bacteria(self.Pdivmax,
+                self.Kx,
+                self.Pdeath,
+                self.resist,
+                self.tol)
+
+    def updatePop(self):
+
+        for i in self.pop:
+
+            if i.divide():
+
+                self.pop.append(bacteria(self.Pdivmax,
+                    self.Kx,
+                    self.Pdeath,
+                    self.resist,
+                    self.tol)
+
+        for i in self.pop:
+
+            if i.death():
+                self.pop(i)
