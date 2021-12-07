7f0ec0941f64f750f90799d028071a471087514f
MaelysMarry <62555143+MaelysMarry@users.noreply.github.com>
2021-12-07 09:17:33 +0100

    Create setup.py

---
+++ src/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import setuptools
+from distutils.core import setup
+
+setup(name='HandsOnDeployAPackage',
+      version='1.7',
+      description='Hands-on: deploy a Python package',
+      author='Maelys',
+      author_email='maelys.marry@insa-lyon.fr'
+      url='https://github.com/MaelysMarry/Projet3BIM',
+      py_modules=['ourCode'],
+      license_files = '../LICENSE',
+      install_requires = ['pandas>=1.3.4', 'matplotlib>=3.5.0'])
