d6b62cccd071cf5d58a104ab4b0e4b5b3318de32
MaelysMarry <62555143+MaelysMarry@users.noreply.github.com>
2021-12-07 09:47:14 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -11,4 +11,4 @@ setup(name='Bacteria',
       url='https://github.com/MaelysMarry/Projet3BIM',
       py_modules=['ourCode'],
       license_files = '../LICENSE',
-      install_requires = ['matplotlib'])
+      install_requires = ['numpy=1.17', 'matplotlib'])
